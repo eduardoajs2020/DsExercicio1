a0aed455fd090c8852936a7ef4eb3148a0a2f54f
eduardoajs2020 <eduardoajs2020@gmail.com>
2020-10-08 22:52:56 -0300

    First

---
+++ backend/src/main/java/com/devexercicio1/devexercicio1/entities/Devexercicio1Application.java
@@ -0,0 +1,13 @@
+package com.devexercicio1.devexercicio1.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devexercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devexercicio1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devexercicio1/devexercicio1/Devexercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devexercicio1.devexercicio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devexercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
